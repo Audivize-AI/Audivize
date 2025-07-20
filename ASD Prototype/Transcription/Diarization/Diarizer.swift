17e5466a5df85dc3e1d638c444fab8a8120589ba
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2025-07-20 11:41:48 -0700

    Re-added Diarizer

--- ASD Prototype/Transcription/Diarization/Diarizer.swift
+++ ASD Prototype/Transcription/Diarization/Diarizer.swift
@@ -6,38 +6,38 @@
 //
 
 import Foundation
-//import FluidAudio
+import FluidAudio
 import CoreMedia
 
 // Initialize and process audio
 
 class Diarizer {
-//    private let diarizer = DiarizerManager()
-//    private var samples: [Float]
+    private let diarizer = DiarizerManager()
+    private var samples: [Float]
     
     init() throws {
-//        self.samples = []
-//        self.samples.reserveCapacity(16000*3)
-//        let diarizer = self.diarizer
-//        Task {
-//            try await diarizer.initialize()
-//        }
+        self.samples = []
+        self.samples.reserveCapacity(16000*3)
+        let diarizer = self.diarizer
+        Task {
+            try await diarizer.initialize()
+        }
     }
     
     
     func diarize(from sampleBuffer: CMSampleBuffer) {
-//        self.samples.append(contentsOf: ASD.resampleAudioToFloat32(from: sampleBuffer, to: 16000))
-//        if self.samples.count > 8000 * 3 {
-//            let diarizer = self.diarizer
-//            let samples = self.samples
-//            Task.detached {
-//                let result = try await diarizer.performCompleteDiarization(samples, sampleRate: 16000)
-//                print("num segments: \(result.segments.count)")
-//                for segment in result.segments {
-//                    print("\(segment.speakerId): \(segment.startTimeSeconds)s - \(segment.endTimeSeconds)s")
-//                }
-//            }
-//            self.samples.removeAll(keepingCapacity: true)
-//        }
+        self.samples.append(contentsOf: ASD.resampleAudioToFloat32(from: sampleBuffer, to: 16000))
+        if self.samples.count > 8000 * 3 {
+            let diarizer = self.diarizer
+            let samples = self.samples
+            Task.detached {
+                let result = try await diarizer.performCompleteDiarization(samples, sampleRate: 16000)
+                print("num segments: \(result.segments.count)")
+                for segment in result.segments {
+                    print("\(segment.speakerId): \(segment.startTimeSeconds)s - \(segment.endTimeSeconds)s")
+                }
+            }
+            self.samples.removeAll(keepingCapacity: true)
+        }
     }
 }
